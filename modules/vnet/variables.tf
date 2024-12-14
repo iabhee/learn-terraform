67ddd14c132106cdc48edea2f8e3601f361a04b7
iabhee <rabhilash.devops@gmail.com>
2024-12-13 19:22:25 -0600

    add network security group and subnet modules

--- modules/vnet/variables.tf
+++ modules/vnet/variables.tf
@@ -1,9 +1,9 @@
 variable "vnet_full_name" {
-  type = string
+  type        = string
   description = <<DESCRIPTION
   (Optionl) The name of the vnet to create.
   DESCRIPTION
-  nullable = false
+  nullable    = false
 }
 
 variable "location" {
@@ -20,4 +20,44 @@ variable "resource_group_name" {
 
 variable "vnet_address_spaces" {
   type = list(string)
+}
+
+variable "subnets" {
+  description = "Option subnets to create within the virtual network"
+  type = map(object({
+    name = string
+    address_prefixes = list(string)
+    network_secruity_group = optional(string)
+    route_table = optional(object({id = string}))
+    service_endpoints = optional(list(string))
+    delegations = optional(list(object({
+      name = string
+      serv_delegation = object({
+        name = string
+        actions = list(string)
+      })
+    })))
+    private_endpoint_network_policies = optional(string)
+    private_link_service_network_policies_enabled = optional(string)
+  }))
+  default = {}
+}
+
+variable "network_secruity_group" {
+  type = list(object({
+    name = string
+    security_rules = list(object({
+      name = string
+      access = string
+      destination_address_prefix = string
+      destination_port_range = string
+      direction = string
+      priority = number
+      protocol = string
+      source_address_prefix = string
+      source_port_range = string
+    }))
+  }))
+  default = []
+  description = "List of Network Security Groups with security rules"
 }
--- modules/vnet/vnet.tf
+++ modules/vnet/vnet.tf
@@ -2,7 +2,36 @@
 resource "azurerm_resource_group" "resource_group_name" {
   location = var.location
   name     = var.resource_group_name
-  tags = var.tags
+  tags     = var.tags
+}
+
+data "http" "public_ip" {
+  url = "https://api.ipify.org?format=json"
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  for_each = { for nsg in var.network_secruity_group : nsg.name => nsg}
+
+  name                = each.value.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  tags                = var.tags
+
+  dynamic "security_rule" {
+    for_each = each.value.security_rules
+    content {
+      name                       = security_rule.value.name
+      priority                   = security_rule.value.priority
+      direction                  = security_rule.value.direction
+      access                     = security_rule.value.access
+      protocol                   = security_rule.value.protocol
+      source_port_range          = security_rule.value.source_port_range
+      destination_port_range     = security_rule.value.destination_port_range
+      source_address_prefix      = security_rule.value.source_address_prefix
+      destination_address_prefix = security_rule.value.destination_address_prefix
+    }    
+  }
+  
 }
 
 module "vnet" {
@@ -12,5 +41,7 @@ module "vnet" {
   name                = var.vnet_full_name
   address_space       = var.vnet_address_spaces
   enable_telemetry    = false
-  tags = azurerm_resource_group.resource_group_name.tags
+  tags                = azurerm_resource_group.resource_group_name.tags
+  depends_on = [ azurerm_resource_group.resource_group_name ]
+  subnets = length(var.subnets) > 0 ? var.subnets : null
 }
